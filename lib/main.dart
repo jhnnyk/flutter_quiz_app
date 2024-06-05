ab4840ac079fecd7f5b803c73bf304541c36b89a
John Knotts <jhnnyk@gmail.com>
2024-06-05 11:50:30 -0600

    show logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/logo.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,14 +9,21 @@ void main() {
           color: Color.fromARGB(255, 92, 5, 133),
         ),
         child: const Center(
-          child: Text(
-            'Hello world',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Hello world',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Logo(),
+            ],
           ),
+
         ),
       ),
     ),
