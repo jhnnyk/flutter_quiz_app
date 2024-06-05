24c280fcf831e0f53ea9eda4bf5c4e6d6f534841
John Knotts <jhnnyk@gmail.com>
2024-06-05 15:06:43 -0600

    starting styles for questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-  
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
@@ -12,7 +12,36 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionsScreen');
-    
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'the question ...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
