7434c561dceeab9083cb1191f08c9eebb6727bf0
John Knotts <jhnnyk@gmail.com>
2024-06-06 13:08:16 -0600

    adding a few styles

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,26 +18,31 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 30),
-          // the spread operator below '...' pulls the items out of
-          // the returned list so that they are separate widgets
-          // the Column widget won't accept a list within a list(it's children)
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {},
-            );
-          }),
-        ],
+            const SizedBox(height: 30),
+            // the spread operator below '...' pulls the items out of
+            // the returned list so that they are separate widgets
+            // the Column widget won't accept a list within a list(it's children)
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {},
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
