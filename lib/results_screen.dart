9275247c94f23d47ecc11439673fb119b1584ac5
John Knotts <jhnnyk@gmail.com>
2024-06-07 10:09:36 -0600

    displaying results screen data

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -32,7 +33,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('You answered x out of y questions correctly!'),
             const SizedBox(height: 30),
-            const Text('list of answers and questions'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30),
             TextButton(
               onPressed: () {},
